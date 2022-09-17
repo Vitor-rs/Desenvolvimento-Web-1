71bd26f7176f57023e896d57fa361188ae3b230d
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2022-09-16 21:31:04 -0400

    up

--- Aulas/Aula09_Dom_Pt2/src/js/dom-parte1.js
+++ Aulas/Aula09_Dom_Pt2/src/js/dom-parte1.js
@@ -1,3 +1,13 @@
 import '@picocss/pico'
 import '../css/global.css'
 import '../css/dom-parte1.css'
+
+let inputDisciplina = document.querySelector("#disciplina")
+let btnAdicionar = document.querySelector(".btn-add")
+let lista = document.querySelector(".lista")
+
+btnAdicionar.onclick = function() {
+
+    alert("Clicou no botão!")
+
+}
