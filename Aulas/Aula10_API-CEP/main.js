39eea9078cbe1ee8ce3dac34b40a066709be5569
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2022-11-04 22:50:08 -0400

    ✨ Adicionando função assíncrona

--- Aulas/Aula10_API-CEP/main.js
+++ Aulas/Aula10_API-CEP/main.js
@@ -1,6 +1,19 @@
 const formConsultarCep = document.querySelector("#consultarCep");
 const inputCep = formConsultarCep.cep;
+const divDados = document.querySelector("#dados");
 
 formConsultarCep.addEventListener("submit", function (event) {
   event.preventDefault(); // anula o comportamento padrão de envio do form
+  consultarCep(inputCep.value);
 });
+
+async function consultarCep(cep) {
+
+  let response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+  let dadosCep = await response.json();
+  
+  divDados.innerHTML = `
+    <p>Endereço: ${dadosCep.logradouro}</p>
+    <p>Localidade: ${dadosCep.localidade}</p>
+  `;
+}
