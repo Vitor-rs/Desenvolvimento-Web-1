0066041f612db38a7e0ea08abc63e0bd4e79493c
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2022-11-18 22:52:01 -0400

    Atualizado

--- Aulas/Aula10_API-CEP/main.js
+++ Aulas/Aula10_API-CEP/main.js
@@ -1,20 +1,40 @@
-import '@picocss/pico'
+import "@picocss/pico";
+import './estilo.css'
+
 const formConsultarCep = document.querySelector("#consultarCep");
 const inputCep = formConsultarCep.cep;
 const divDados = document.querySelector("#dados");
+/* const loader = `<a href="#" aria-busy="true">Consultando o CEP, aguarde...</a>`; */
+const btnConsultarCep = document.querySelector("#btnConsultarCep");
 
 formConsultarCep.addEventListener("submit", function (event) {
   event.preventDefault(); // anula o comportamento padrão de envio do form
+  ativaLoader(true);
   consultarCep(inputCep.value);
 });
 
-async function consultarCep(cep) {
 
+async function consultarCep(cep) {
   let response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
   let dadosCep = await response.json();
-  
-  divDados.innerHTML = `
-    <p>Endereço: ${dadosCep.logradouro}</p>
-    <p>Localidade: ${dadosCep.localidade}</p>
-  `;
+
+  if (dadosCep.erro) {
+    divDados.innerHTML = `<div class='erro'>CEP não encontrado!</div>`;
+  } else {
+    divDados.innerHTML = `
+      <p>Endereço: ${dadosCep.logradouro}</p>
+      <p>Localidade: ${dadosCep.localidade}</p>
+    `;
+  }
+  ativaLoader(false);
+}
+
+function ativaLoader(ativo) {
+  if (ativo) {
+    btnConsultarCep.setAttribute("aria-busy", "true");
+    btnConsultarCep.textContent = "Consultando o CEP...";
+  } else {
+    btnConsultarCep.removeAttribute("aria-busy");
+    btnConsultarCep.textContent = "Consultar";
+  }
 }
