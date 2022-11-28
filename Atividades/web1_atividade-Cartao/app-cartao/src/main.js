0fbb3ade77e74864102e6267c9c311f32d762df5
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2022-11-28 00:29:32 -0400

    🚧 Declarando variáveis

--- Atividades/web1_atividade-Cartao/app-cartao/src/main.js
+++ Atividades/web1_atividade-Cartao/app-cartao/src/main.js
@@ -1,2 +1,12 @@
-import '/style.css'
+const form = document.querySelector("#cartao");
+
+const cardNumber = document.querySelector("#numero-cartao");
+const cardHolder = document.querySelector("#texto-nome");
+const cardExpiration = document.querySelector("#texto-valido-ate");
+const cardCVV = document.querySelector("#texto-cvc");
+
+const cardNumberText = document.querySelector(".numeroValor");
+const cardHolderText = document.querySelector(".nomeValor");
+const cardExpirationText = document.querySelector(".validade-ate");
+const cardCVVText = document.querySelector(".cvcValor");
 
