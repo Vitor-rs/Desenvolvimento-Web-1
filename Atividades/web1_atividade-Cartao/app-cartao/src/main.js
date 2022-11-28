ddd641a5ce9f1805414dff626de2ff014ca1b262
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2022-11-28 09:35:20 -0400

    🐛 Ajustando o código

--- Atividades/web1_atividade-Cartao/app-cartao/src/main.js
+++ Atividades/web1_atividade-Cartao/app-cartao/src/main.js
@@ -11,38 +11,27 @@ const validadeCartaoText = document.querySelector(".validade-ate");
 const cvcCartaoText = document.querySelector(".cvcValor");
 
 
-numCartao.addEventListener("keyup", (e) => { // Expressões regulares para mascarare validar os inputs
+numCartao.addEventListener("keyup", (e) => { // Expressões regulares para mascarar e validar os inputs
   if (!e.target.value) {
     numCartaoText.innerText = "0000 0000 0000 0000";
   } else {
     const valuesOfInput = e.target.value.replaceAll(" ", "");
 
     if (e.target.value.length > 14) {
-      e.target.value = valuesOfInput.replace(
-        /(\d{4})(\d{4})(\d{4})(\d{0,4})/,
-        "$1 $2 $3 $4"
-      );
-      numCartaoText.innerHTML = valuesOfInput.replace(
-        /(\d{4})(\d{4})(\d{4})(\d{0,4})/,
-        "$1 $2 $3 $4"
-      );
-    } else if (e.target.value.length > 9) {
-      e.target.value = valuesOfInput.replace(
-        /(\d{4})(\d{4})(\d{0,4})/,
-        "$1 $2 $3"
-      );
-      numCartaoText.innerHTML = valuesOfInput.replace(
-        /(\d{4})(\d{4})(\d{0,4})/,
-        "$1 $2 $3"
-      );
-    } else if (e.target.value.length > 4) {
+      e.target.value = valuesOfInput.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/,"$1 $2 $3 $4");
+      numCartaoText.innerHTML = valuesOfInput.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
+    }
+    else if (e.target.value.length > 9) {
+      e.target.value = valuesOfInput.replace(/(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3");
+      numCartaoText.innerHTML = valuesOfInput.replace(/(\d{4})(\d{4})(\d{0,4})/,"$1 $2 $3");
+    }
+    else if (e.target.value.length > 4) {
       e.target.value = valuesOfInput.replace(/(\d{4})(\d{0,4})/, "$1 $2");
-      numCartaoText.innerHTML = valuesOfInput.replace(
-        /(\d{4})(\d{0,4})/,
-        "$1 $2"
-      );
-    } else {
+      numCartaoText.innerHTML = valuesOfInput.replace(/(\d{4})(\d{0,4})/, "$1 $2");
+    }
+    else {
       numCartaoText.innerHTML = valuesOfInput;
     }
   }
 });
+
