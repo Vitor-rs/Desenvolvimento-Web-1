919ec3de0a9b4108c293d9d2ce25b195afeac244
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2022-12-02 19:35:45 -0400

    Ajustando algumas partes

--- Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
+++ Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
@@ -31,15 +31,15 @@ const criarCartaoUsuario = (usuario) => {
 };
 
 // método para retornar o tipo de erro caso a api falhe
-const criarCartoDeErro = (mensagem) => {
+function criarCartoDeErro(mensagem) {
     const cartaoHTML = `<div class="cartaoErro"><h1>${mensagem}</h1></div>`;
     main.innerHTML = cartaoHTML;
 }
 
 // método para adicionar o repositório ao cartao
-const addReposNoCartao = (mensagem) => {
+function addReposNoCartao(mensagem) {
     const elementoDoRepos = document.querySelector("repos");
-    repos.slice(0,5).forEach((repo) => {
+    repos.slice(0, 5).forEach((repo) => {
         const elementoRepo = document.createElement('a');
         elementoRepo.classList.add("repo");
         elementoRepo.href = repo.html_url;
@@ -51,24 +51,24 @@ const addReposNoCartao = (mensagem) => {
 
 
 // método para captar o repositório
-const getRepositorio = async (nome_usuario) => {
-    try {//
-        const {dados} = await axios(APIGITHUB + nome_usuario)
+async function getRepositorio(nome_usuario) {
+    try { //
+        const { dados } = await axios(APIGITHUB + nome_usuario);
         addReposNoCartao(dados);
     } catch (error) {
-        criarCartoDeErro("Problemas ao procurar o repositório")
+        criarCartoDeErro("Problemas ao procurar o repositório");
     }
 }
 
 // método para pegar o usuário
-const getUsuario = async (nome_usuario) => {
+async function getUsuario(nome_usuario) {
     try { // aqui estou usando o axios para fazer um fetch da api com tratamento de erro
-        const {dados} = await axios(APIGITHUB + nome_usuario)
+        const { dados } = await axios(APIGITHUB + nome_usuario);
         criarCartaoUsuario(dados);
         getRepositorio(nome_usuario);
     } catch {
-    if (error.response.status == 404)
-        criarCartoDeErro("Perfil inexistente!")
+        if (error.response.status == 404)
+            criarCartoDeErro("Perfil inexistente!");
     }
 }
 
