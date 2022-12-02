b0ccc870568925a3410cc8a13343cf219a750319
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2022-12-02 18:20:56 -0400

    Tratamento de erro da API

--- Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
+++ Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
@@ -1,4 +1,5 @@
 import "@picocss/pico";
+import axios from "axios";
 
 // recebendo a API em uma constante
 const APIGITHUB = "https://api.github.com/users/"
@@ -7,7 +8,15 @@ const form = document.querySelector("form");
 const main = document.querySelector("main");
 const pesquisar = document.querySelector("pesquisar");
 
-// método para pegar o usuário
 
+
+// método para pegar o usuário
+const getUsuario = async (nome_usuario) => {
+    try { // aqui estou usando o axios para fazer um fetch da api com tratamento de erro
+        const { dados } = await axios(APIGITHUB + nome_usuario)
+    } catch {
+
+    }
+}
 
 
