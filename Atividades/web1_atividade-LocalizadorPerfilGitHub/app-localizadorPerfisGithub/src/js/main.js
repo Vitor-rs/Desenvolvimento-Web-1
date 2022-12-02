b298b5211a662a64106bc01741d73ceb01fd59be
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2022-12-02 18:23:03 -0400

    Método para injetar uma template string do card do usuário

--- Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
+++ Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
@@ -8,7 +8,12 @@ const form = document.querySelector("form");
 const main = document.querySelector("main");
 const pesquisar = document.querySelector("pesquisar");
 
-
+// método para injetar uma template string do card do usuário
+const criarCartaoUsuario = (usuario) => {
+    const cartaoHTML = `
+        
+    `;
+}
 
 // método para pegar o usuário
 const getUsuario = async (nome_usuario) => {
