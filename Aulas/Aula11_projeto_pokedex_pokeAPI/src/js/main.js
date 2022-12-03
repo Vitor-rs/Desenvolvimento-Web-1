3e673f5d1f76f61438291ae0b8cf819403b36639
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2022-12-03 14:44:32 -0400

    Push projeto PokeDex

---
+++ Aulas/Aula11_projeto_pokedex_pokeAPI/src/js/main.js
@@ -0,0 +1,34 @@
+import "../css/style.css";
+
+/* Selecionando os elementos do DOM */
+const inputPesquisa = document.querySelector("#inputPesquisa");
+const btnLocalizar = document.querySelector("#btnLocalizar");
+const pokedexDisplay = document.querySelector("#display");
+
+/* Adicionando eventos do DOM */ /* função assíncrona para */
+btnLocalizar.addEventListener("click", async function () {
+  /* buscar os dados da pokemon na API */
+  const dadosDoPokemon = await localizarPokemon(inputPesquisa.value);
+  /* criar o cartão do pokemon */
+  criarCartao(dadosDoPokemon);
+});
+
+async function localizarPokemon(termoBusca) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${termoBusca}`;
+  const response = await fetch(url); /* esperando a resposta da API */
+  const pokemon = await response.json(); /* convertendo a resposta para JSON sendo uma função*/
+  console.log(pokemon)
+  return pokemon;
+}
+
+function criarCartao(pokemon) {
+    /* criando elemento container div */
+    const cartaoPokemon = document.createElement("div");
+    cartaoPokemon.className = "cartaoPokemon";
+
+    cartaoPokemon.innerHTML = `
+        <img class="pokemonSprite" src="${pokemon.sprites.front_default}"/>
+        <h2>${pokemon.name}</h2>
+    `;
+    pokedexDisplay.appendChild(cartaoPokemon);
+}
