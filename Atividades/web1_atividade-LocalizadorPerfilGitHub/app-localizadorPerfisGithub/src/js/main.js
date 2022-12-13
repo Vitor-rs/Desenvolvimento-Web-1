df4ddc8bd2e106ccdd70260ee878f8fafc06a2db
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2022-12-13 12:14:36 -0400

    🔥 Removendo parte do script

--- Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
+++ Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
@@ -1,4 +1,4 @@
-// import "@picocss/pico";
+import "@picocss/pico";
 
 // recebendo a API em uma constante
 const APIGITHUB = "https://api.github.com/users/";
@@ -14,67 +14,3 @@ async function getUsuario(usuario) {
   return getInfoUsuario(responseDado);
 }
 
-function getErro(response) {
-  const usuarioErro = `
-    <div class="usuario-box">
-            <div class="foto-avatar">
-                    <img src="./img/empty.png" class="avatar" alt="Indefinido"></img>
-            </div>
-            <div class="info-usuario">
-                <h2>Usuário inexistente</h2>
-                <div class="bio-usuario">
-                    <p>${usuario.bio}</p>
-                    <a href="${usuario.html_url}" target="_blank">Ver perfil ></a>
-                </div>
-                <ul class="meta-dados">
-                    <li>${usuario.followers} <strong>Seguidores</strong></li>
-                    <li>${usuario.following} <strong>Seguindo</strong></li>
-                    <li>${usuario.public_repos} <strong>Repositórios</strong></li>
-                </ul>
-            </div>
-        </div>
-    `;
-  perfil.innerHTML = usuarioErro;
-}
-
-// Função para mostrar o resultado
-function getInfoUsuario(usuario) {
-  const usuarioBox = `
-        <div class="usuario-box">
-            <div class="foto-avatar">
-                <a href="${usuario.html_url}" target="_blank">
-                    <img src="${usuario.avatar_url}" class="avatar" alt="${usuario.name}"></img>
-                </a>
-            </div>
-            <div class="info-usuario">
-                <h2>${usuario.name}</h2>
-                <div class="bio-usuario">
-                    <p>${usuario.bio}</p>
-                    <a href="${usuario.html_url}" target="_blank">Ver perfil ></a>
-                </div>
-                <ul class="meta-dados">
-                    <li>${usuario.followers} <strong>Seguidores</strong></li>
-                    <li>${usuario.following} <strong>Seguindo</strong></li>
-                    <li>${usuario.public_repos} <strong>Repositórios</strong></li>
-                </ul>
-            </div>
-        </div>
-    `;
-  perfil.innerHTML = usuarioBox;
-}
-
-form.addEventListener("submit", async (e) => {
-  //e.preventDefault();
-  const valor_pesquisa = pesquisa.value;
-  const resultado_pesquisa = await getUsuario(valor_pesquisa);
-  console.log(resultado_pesquisa);
-  if (!resultado_pesquisa.login) {
-    alert('Indefinido')
-  } else { // parei aqui
-    if (valor_pesquisa) {
-      getUsuario(valor_pesquisa);
-      pesquisa.value = "";
-    }
-  }
-  
-});
