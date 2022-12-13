529861e94304356f636eb3f6d7688b37b34add4d
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2022-12-12 23:36:28 -0400

    correções

--- Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
+++ Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
@@ -5,7 +5,7 @@ const APIGITHUB = "https://api.github.com/users/";
 
 const perfil = document.querySelector("#perfil-Usuario");
 const form = document.querySelector("#form");
-const pesquisa = document.querySelector("pesquisaBox");
+const pesquisa = document.querySelector("#pesquisaBox");
 
 // Função que capta a API
 async function getUsuario(usuario) {
