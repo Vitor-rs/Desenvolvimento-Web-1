6f8adb27267bb03898bd6610cacbaddcd48f133a
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2022-12-12 23:21:00 -0400

    🚩 JS função de captar a API

--- Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
+++ Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
@@ -3,9 +3,15 @@ import "@picocss/pico";
 // recebendo a API em uma constante
 const APIGITHUB = "https://api.github.com/users/";
 
-const perfilUsuario = document.querySelector("#perfil-Usuario");
+const perfil = document.querySelector("#perfil-Usuario");
 const form = document.querySelector("#form");
 const pesquisa = document.querySelector("pesquisaBox");
+
+// Função que capta a API
+async function gerUsuario(usuario) {
+    const response = await fetch(APIGITHUB + usuario);
+    const responseDado = await response.json();
+}
 
 
 
