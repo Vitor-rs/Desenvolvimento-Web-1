fd8d0a20543a3f37a1dfd0a2273f91518a29e212
VitorGit93 <vitor.santos9@estudante.ifms.edu.br>
2022-12-13 15:32:36 -0400

    Refazendo o JS

--- Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
+++ Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
@@ -3,9 +3,10 @@ import "@picocss/pico";
 // recebendo a API em uma constante
 const APIGITHUB = "https://api.github.com/users/";
 
-const perfil = document.querySelector("#perfil-Usuario");
-const form = document.querySelector("#form");
-const pesquisa = document.querySelector("#pesquisaBox");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+const card = document.querySelector(".card");
+
 
 // Função que capta a API
 /*async function getUsuario(usuario) {
