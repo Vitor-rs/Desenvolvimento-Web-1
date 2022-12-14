bfb108541d7e2c123abf91e4159f24b5cd329986
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2022-12-14 03:55:19 -0400

    ➕ 🚧 Criando constantes dos atributos customizados

--- Atividades/web1_atividade-PokeDex/src/js/script.js
+++ Atividades/web1_atividade-PokeDex/src/js/script.js
@@ -1,1 +1,10 @@
 import "../css/style.css";
+
+const pokeCard = document.querySelector("[dado-poke-card]");
+const pokeNome = document.querySelector("[dado-poke-nome]");
+const pokeImg = document.querySelector("[dado-poke-img]");
+const pokeImgContainer = document.querySelector("[dado-poke-img-container]");
+const pokeId = document.querySelector("[dado-poke-id]");
+const pokeTipos = document.querySelector("[dado-poke-tipos]");
+const pokeStats = document.querySelector("[dado-poke-stats]");
+
