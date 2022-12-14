8165a49a83e0f8ec05fd4a05a6a07ecbffef775b
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2022-12-13 23:38:00 -0400

    Código estável e funcionando

--- Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
+++ Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
@@ -2,8 +2,6 @@ const input = document.querySelector("input");
 const btn = document.querySelector("button");
 const card = document.querySelector(".card");
 
-const repos_container = document.querySelector(".repos");
-
 // Recebendo a API do Github do usuário
 async function usuario(usuario) {
   const response = await fetch(`https://api.github.com/users/${usuario}`);
@@ -11,33 +9,10 @@ async function usuario(usuario) {
   return responseDados; // a const responseDados irá para a const resultadoPesquisa
 }
 
-//
-// Recebendo a API do Github dos repositórios do usuário
-/*async function repos(usuario) { 
-  const response = await fetch(`https://api.github.com/users/${usuario}/repos`);
-  const responseDados = await response.json();
-  return responseDados; // a const responseDados irá para a const resultadoPesquisa
-}
-
-
-
-const perfil = document.querySelector("#perfil-Usuario");
-const form = document.querySelector("#form");
-const pesquisa = document.querySelector("#pesquisaBox");
-
-// Função que capta a API
-/*async function getUsuario(usuario) {
-  const response = await fetch(APIGITHUB + usuario);
-  const responseDado = await response.json();
-  return getInfoUsuario(responseDado);
-}*/
-
 // Botão que aciona toda a cadeia de eventos
 btn.addEventListener("click", async () => {
   const valor_input = input.value;
   const resultadoPesquisa = await usuario(valor_input); // recebe a const responseDados da função
-
-  /*add_repo();*/
 
   // Deixei o console.log só para analisar o código ao inspecionar
   console.log(resultadoPesquisa);
@@ -64,7 +39,7 @@ btn.addEventListener("click", async () => {
                 </div>
                 <div class="elemento">
                     <span>${resultadoPesquisa.public_repos}</span>
-                    <span>Respositório</span>
+                    <span>Respositório(s)</span>
                 </div>
             </div>
             <a href="${resultadoPesquisa.html_url}" target="_blank">Visitar perfil > </a>
