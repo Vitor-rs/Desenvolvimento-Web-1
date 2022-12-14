ad451c3102fb4c33127accae7644739a685cf77c
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2022-12-14 05:10:38 -0400

    💄 CSS poke-card:before

--- Atividades/web1_atividade-PokeDex/src/js/script.js
+++ Atividades/web1_atividade-PokeDex/src/js/script.js
@@ -1,100 +1,94 @@
 import "../css/style.css";
 
 // Aqui usei o recurso de atributo customizado
-const pokeCard = document.querySelector("[data-poke-card]");
-const pokeName = document.querySelector("[data-poke-name]");
-const pokeImg = document.querySelector("[data-poke-img]");
-const pokeImgContainer = document.querySelector("[data-poke-img-container]");
-const pokeId = document.querySelector("[data-poke-id]");
-const pokeTypes = document.querySelector("[data-poke-types]");
-const pokeStats = document.querySelector("[data-poke-stats]");
+const pokeCard = document.querySelector('[data-poke-card]');
+const pokeName = document.querySelector('[data-poke-name]');
+const pokeImg = document.querySelector('[data-poke-img]');
+const pokeImgContainer = document.querySelector('[data-poke-img-container]');
+const pokeId = document.querySelector('[data-poke-id]');
+const pokeTypes = document.querySelector('[data-poke-types]');
+const pokeStats = document.querySelector('[data-poke-stats]');
 
-// Vetor que contém as cores dos tipos de pokemom
 const typeColors = {
-  electric: "#FFEA70",
-  normal: "#B09398",
-  fire: "#FF675C",
-  water: "#0596C7",
-  ice: "#AFEAFD",
-  rock: "#999799",
-  flying: "#7AE7C7",
-  grass: "#4A9681",
-  psychic: "#FFC6D9",
-  ghost: "#561D25",
-  bug: "#A2FAA3",
-  poison: "#795663",
-  ground: "#D2B074",
-  dragon: "#DA627D",
-  steel: "#1D8A99",
-  fighting: "#2F2F2F",
-  default: "#2A1A1F",
+    electric: '#FFEA70',
+    normal: '#B09398',
+    fire: '#FF675C',
+    water: '#0596C7',
+    ice: '#AFEAFD',
+    rock: '#999799',
+    flying: '#7AE7C7',
+    grass: '#4A9681',
+    psychic: '#FFC6D9',
+    ghost: '#561D25',
+    bug: '#A2FAA3',
+    poison: '#795663',
+    ground: '#D2B074',
+    dragon: '#DA627D',
+    steel: '#1D8A99',
+    fighting: '#2F2F2F',
+    default: '#2A1A1F',
 };
 
-// Função que capta a API do Pokemon
-const getPokemon = (event) => {
-  event.preventDefault();
-  const { value } = event.target.pokemon;
-  fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
-    .then((data) => data.json())
-    .then((response) => renderPokemonData(response))
-    .catch((err) => renderNotFound());
-};
 
-// Função para captar os dados do Pokemon
-const renderPokemonData = (data) => {
-  const sprite = data.sprites.front_default;
-  const { stats, types } = data;
+const searchPokemon = event => {
+    event.preventDefault();
+    const { value } = event.target.pokemon;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
+        .then(data => data.json())
+        .then(response => renderPokemonData(response))
+        .catch(err => renderNotFound())
+}
 
-  pokeName.textContent = data.name;
-  pokeImg.setAttribute("src", sprite);
-  pokeId.textContent = `Nº ${data.id}`;
-  setCardColor(types);
-  renderPokemonTypes(types);
-  renderPokemonStats(stats);
-};
+const renderPokemonData = data => {
+    const sprite =  data.sprites.front_default;
+    const { stats, types } = data;
 
-// Função para setar as cores correspondentes aos Pokemons
-const setCardColor = (types) => {
-  const colorOne = typeColors[types[0].type.name];
-  const colorTwo = types[1]
-    ? typeColors[types[1].type.name]
-    : typeColors.default;
-  pokeImg.style.background = `radial-gradient(${colorTwo} 33%, ${colorOne} 33%)`;
-  pokeImg.style.backgroundSize = " 5px 5px";
-};
+    pokeName.textContent = data.name;
+    pokeImg.setAttribute('src', sprite);
+    pokeId.textContent = `Nº ${data.id}`;
+    setCardColor(types);
+    renderPokemonTypes(types);
+    renderPokemonStats(stats);
+}
 
-// Função para renderizar os tipos de Pokemon
-const renderPokemonTypes = (types) => {
-  pokeTypes.innerHTML = "";
-  types.forEach((type) => {
-    const typeTextElement = document.createElement("div");
-    typeTextElement.style.color = typeColors[type.type.name];
-    typeTextElement.textContent = type.type.name;
-    pokeTypes.appendChild(typeTextElement);
-  });
-};
 
-// Função para renderizar os stats do Pokemon
-const renderPokemonStats = (stats) => {
-  pokeStats.innerHTML = "";
-  stats.forEach((stat) => {
-    const statElement = document.createElement("div");
-    const statElementName = document.createElement("div");
-    const statElementAmount = document.createElement("div");
-    statElementName.textContent = stat.stat.name;
-    statElementAmount.textContent = stat.base_stat;
-    statElement.appendChild(statElementName);
-    statElement.appendChild(statElementAmount);
-    pokeStats.appendChild(statElement);
-  });
-};
+const setCardColor = types => {
+    const colorOne = typeColors[types[0].type.name];
+    const colorTwo = types[1] ? typeColors[types[1].type.name] : typeColors.default;
+    pokeImg.style.background =  `radial-gradient(${colorTwo} 33%, ${colorOne} 33%)`;
+    pokeImg.style.backgroundSize = ' 5px 5px';
+}
 
-// Função para mostrar aviso de Pokemon inexistente
+const renderPokemonTypes = types => {
+    pokeTypes.innerHTML = '';
+    types.forEach(type => {
+        const typeTextElement = document.createElement("div");
+        typeTextElement.style.color = typeColors[type.type.name];
+        typeTextElement.textContent = type.type.name;
+        pokeTypes.appendChild(typeTextElement);
+    });
+}
+
+const renderPokemonStats = stats => {
+    pokeStats.innerHTML = '';
+    stats.forEach(stat => {
+        const statElement = document.createElement("div");
+        const statElementName = document.createElement("div");
+        const statElementAmount = document.createElement("div");
+        statElementName.textContent = stat.stat.name;
+        statElementAmount.textContent = stat.base_stat;
+        statElement.appendChild(statElementName);
+        statElement.appendChild(statElementAmount);
+        pokeStats.appendChild(statElement);
+    });
+}
+
 const renderNotFound = () => {
-  pokeName.textContent = "No encontrado";
-  pokeImg.setAttribute("src", "poke-shadow.png");
-  pokeImg.style.background = "#fff";
-  pokeTypes.innerHTML = "";
-  pokeStats.innerHTML = "";
-  pokeId.textContent = "";
-};
+    pokeName.textContent = 'No encontrado';
+    pokeImg.setAttribute('src', 'poke-shadow.png');
+    pokeImg.style.background =  '#fff';
+    pokeTypes.innerHTML = '';
+    pokeStats.innerHTML = '';
+    pokeId.textContent = '';
+}
+
