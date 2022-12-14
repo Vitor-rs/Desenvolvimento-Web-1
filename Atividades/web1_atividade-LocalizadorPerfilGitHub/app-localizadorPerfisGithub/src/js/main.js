8e48402316379a66009c29200be378f280470d5a
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2022-12-13 23:31:00 -0400

    🚧 Ajustando nomes de tags

--- Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
+++ Atividades/web1_atividade-LocalizadorPerfilGitHub/app-localizadorPerfisGithub/src/js/main.js
@@ -5,18 +5,18 @@ const card = document.querySelector(".card");
 const repos_container = document.querySelector(".repos");
 
 // Recebendo a API do Github do usuário
-async function user(username) {
-  const resp = await fetch(`https://api.github.com/users/${username}`);
-  const respData = await resp.json();
-  return respData; // a const respData irá para a const search_result
+async function usuario(usuario) {
+  const response = await fetch(`https://api.github.com/users/${usuario}`);
+  const responseDados = await response.json();
+  return responseDados; // a const responseDados irá para a const resultadoPesquisa
 }
 
 //
 // Recebendo a API do Github dos repositórios do usuário
-/*async function repos(username) { 
-  const resp = await fetch(`https://api.github.com/users/${username}/repos`);
-  const respData = await resp.json();
-  return respData; // a const respData irá para a const search_result
+/*async function repos(usuario) { 
+  const response = await fetch(`https://api.github.com/users/${usuario}/repos`);
+  const responseDados = await response.json();
+  return responseDados; // a const responseDados irá para a const resultadoPesquisa
 }
 
 
@@ -34,40 +34,40 @@ const pesquisa = document.querySelector("#pesquisaBox");
 
 // Botão que aciona toda a cadeia de eventos
 btn.addEventListener("click", async () => {
-  const input_val = input.value;
-  const search_result = await user(input_val); // recebe a const respData da função
+  const valor_input = input.value;
+  const resultadoPesquisa = await usuario(valor_input); // recebe a const responseDados da função
 
   /*add_repo();*/
 
   // Deixei o console.log só para analisar o código ao inspecionar
-  console.log(search_result);
+  console.log(resultadoPesquisa);
 
-  if (!search_result.login) {
-    alert("User not found");
+  if (!resultadoPesquisa.login) {
+    alert("Usuário inexistente");
   } else {
     card.innerHTML = `
             <div class="avatar">
-                <img src="${search_result.avatar_url}" alt="">
+                <img src="${resultadoPesquisa.avatar_url}" alt="">
             </div>
             <div class="info">
-                <h2>${search_result.name}</h2>
-                <p>${search_result.login}</p>
-                <div class="follow-info">
-                    <div class="single">
-                        <span>${search_result.followers}</span>
-                        <span>Followers</span>
+                <h2>${resultadoPesquisa.name}</h2>
+                <p>${resultadoPesquisa.login}</p>
+                <div class="info-seguir">
+                    <div class="elemento">
+                        <span>${resultadoPesquisa.followers}</span>
+                        <span>Seguidores</span>
                     </div>
-                    <div class="single">
-                        <span>${search_result.following}</span>
-                        <span>Following</span>
+                    <div class="elemento">
+                        <span>${resultadoPesquisa.following}</span>
+                        <span>Seguindo</span>
                     </div>
                 </div>
-                <div class="single">
-                    <span>${search_result.public_repos}</span>
-                    <span>Respos</span>
+                <div class="elemento">
+                    <span>${resultadoPesquisa.public_repos}</span>
+                    <span>Respositório</span>
                 </div>
             </div>
-            <a href="${search_result.html_url}" target="_blank">Visitar repositorio > </a>
+            <a href="${resultadoPesquisa.html_url}" target="_blank">Visitar perfil > </a>
         `;
   }
 });
